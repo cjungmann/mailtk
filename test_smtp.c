19805887980dd71427a2ea1144d86f10c102290b
Chuck Jungmann <chuckj60@gmail.com>
2020-02-21 18:05:27 -0600

    Save changes before I remove the file from the project.

--- test_smtp.c
+++ test_smtp.c
@@ -10,7 +10,7 @@ void tls_user(STalker *stalker, void *data)
    ServerCreds *sc = (ServerCreds*)data;
    SMTPCaps scaps;
 
-   if (greet_smtp_server(&scaps, sc, stalker))
+   if (greet_smtp_server(&scaps, sc->host_url, stalker))
    {
       printf("Established a TLS SMTP connection.\nThese are the available SMTP Caps:\n");
       show_smtpcaps(&scaps);
@@ -24,10 +24,10 @@ void socket_user(STalker *stalker, void *data)
    ServerCreds *sc = (ServerCreds*)data;
    SMTPCaps scaps;
 
-   if (greet_smtp_server(&scaps, sc, stalker))
+   if (greet_smtp_server(&scaps, sc->host_url, stalker))
    {
       if (scaps.cap_starttls)
-         start_tls(sc, stalker, tls_user);
+         start_tls(tls_user, stalker, data);
       else
       {
          printf("Using SMTP server without logging into it.\nThese are the available SMTP Caps:\n");
@@ -45,7 +45,7 @@ int main(int argc, const char **argv)
    const char *host_url = sc.host_url;
    int         host_port = sc.host_port;
 
-   int exit_code = open_socket_talker(host_url, host_port, socket_user, &sc);
+   int exit_code = open_socket_talker(socket_user, host_url, host_port, &sc);
    if (exit_code)
       printf("Failed to open socket talker, %d.\n", exit_code);
 
