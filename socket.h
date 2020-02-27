20e9dc24f58e3eba19359e3e74245aae6717880e
Chuck Jungmann <chuckj60@gmail.com>
2020-02-27 08:23:20 -0600

    Removed unused typedef.

--- socket.h
+++ socket.h
@@ -5,11 +5,6 @@
 
 typedef void (*talker_user)(STalker *talker, void *data);
 
-typedef struct _socket_handle
-{
-   STalker talker;
-} SockHandle;
-
 int open_socket_talker(talker_user callback, const char *host_url, int host_port, void *data);
 void open_ssl_talker(talker_user callback, STalker *open_talker, void *data);
 
